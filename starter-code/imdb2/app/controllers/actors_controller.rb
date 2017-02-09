45df0b47fe6259da48a66f4d918e656942a33043
cavacado <ngzhaoloong@gmail.com>
2017-02-09 13:57:57 +0800

    hopefully this will cause a merge conflict

--- starter-code/imdb2/app/controllers/actors_controller.rb
+++ starter-code/imdb2/app/controllers/actors_controller.rb
@@ -1,3 +1,22 @@
 class ActorsController < ApplicationController
+  def show
+    def new_method
+      puts 'meow meow meow'
+    end
+  end
+
+  def new
+    puts 'lalalala'
+  end
+
+  def create
+    puts 'this will cause many merge conflicts!'
+  end
+
+  def edit
+  end
+
+  def update
+  end
 
 end
--- starter-code/imdb2/app/controllers/movies_controller.rb
+++ starter-code/imdb2/app/controllers/movies_controller.rb
@@ -1,3 +1,24 @@
 class MoviesController < ApplicationController
+  def index
+  end
+
+  def show
+    def another_method
+      puts 'this is not very conducive'
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    puts 'create another bracnhhhhhh'
+  end
+
+  def update
+  end
 
 end
